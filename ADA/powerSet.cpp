fde4a4b0d50cb41ee5113c8b5ee5b89046954246
Danny-Wits <dannywits2003@gmail.com>
2026-01-28 20:57:52 +0530

    fixed the power set question

--- ADA/powerSet.cpp
+++ ADA/powerSet.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
-#include <vector>
+#include <set>
+#include <iterator>
 using namespace std;
-int count = 1;
-void printVector(vector<int> list)
+void printSet(set<int> s)
 {
-    if (list.size() == 0)
+    cout << "[ ";
+    for (int item : s)
     {
-        return;
+        cout << item << ",";
     }
-    ++::count;
-    cout << "[";
-    for (int s : list)
-    {
-        cout << s << ",";
-    }
-    cout << "\b],";
+    cout << "\b ],";
 }
-//todo : fix it 
-int powerSet(vector<int> v)
+// todo : fix it
+
+set<set<int>> s;
+int powerSet(set<int> v)
 {
-    printVector(v);
+    s.insert(v);
     for (int i = 0; i < v.size(); i++)
     {
-        vector<int> temp = v;
-        temp.erase(temp.begin() + i);
+        set<int> temp = v;
+        temp.erase(next(temp.begin(), i));
         powerSet(temp);
     }
 }
 
 int main()
 {
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
+    set<int> v;
+    v.insert(1);
+    v.insert(2);
+    v.insert(3);
+    v.insert(4);
+    v.insert(5);
     cout << "Vector : ";
-    printVector(v);
-    cout << endl;
+    printSet(v);
     powerSet(v);
-    cout << "\nNumber of subsets: " << ::count - 1 << endl;
+    cout << "\nNumber of subsets: " << s.size() << endl;
+    cout << "Power set : [ ";
+    for (auto i : s)
+    {
+        printSet(i);
+    }
+    cout << "]";
+
     return 0;
 }
